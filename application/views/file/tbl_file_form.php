042c1d8feae10d2a08dd9a5fb23a2f5d5d6bfb7a
M. Rasyid Ridha <mrasyidridha532@gmail.com>
2020-07-17 14:18:18 +0700

    Ganti template file

--- application/views/file/tbl_file_form.php
+++ application/views/file/tbl_file_form.php
@@ -1,32 +1,31 @@
 <!doctype html>
 <html>
-    <head>
-        <title>harviacode.com - codeigniter crud generator</title>
-        <link rel="stylesheet" href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css') ?>"/>
-        <style>
-            body{
-                padding: 15px;
-            }
-        </style>
-    </head>
-    <body>
-        <h2 style="margin-top:0px">Tbl_file <?php echo $button ?></h2>
-        <form action="<?php echo $action; ?>" method="post">
-	    <div class="form-group">
+
+<head>
+    <title>harviacode.com - codeigniter crud generator</title>
+    <link rel="stylesheet" href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css') ?>" />
+    <style>
+        body {
+            padding: 15px;
+        }
+    </style>
+</head>
+
+<body>
+    <h2 style="margin-top:0px">Tbl_file <?php echo $button ?></h2>
+    <form action="<?php echo $action; ?>" method="post">
+        <div class="form-group">
             <label for="varchar">Judul <?php echo form_error('judul') ?></label>
             <input type="text" class="form-control" name="judul" id="judul" placeholder="Judul" value="<?php echo $judul; ?>" />
         </div>
-	    <div class="form-group">
+        <div class="form-group">
             <label for="varchar">Nama File <?php echo form_error('nama_file') ?></label>
             <input type="text" class="form-control" name="nama_file" id="nama_file" placeholder="Nama File" value="<?php echo $nama_file; ?>" />
         </div>
-	    <div class="form-group">
-            <label for="varchar">Link <?php echo form_error('link') ?></label>
-            <input type="text" class="form-control" name="link" id="link" placeholder="Link" value="<?php echo $link; ?>" />
-        </div>
-	    <input type="hidden" name="id_file" value="<?php echo $id_file; ?>" /> 
-	    <button type="submit" class="btn btn-primary"><?php echo $button ?></button> 
-	    <a href="<?php echo site_url('file') ?>" class="btn btn-default">Cancel</a>
-	</form>
-    </body>
+        <input type="hidden" name="id_file" value="<?php echo $id_file; ?>" />
+        <button type="submit" class="btn btn-primary"><?php echo $button ?></button>
+        <a href="<?php echo site_url('file') ?>" class="btn btn-default">Cancel</a>
+    </form>
+</body>
+
 </html>
