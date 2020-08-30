1b741c4de9dc3d1960c462dc60014998081f46b2
M. Rasyid Ridha <mrasyidridha532@gmail.com>
2020-08-30 21:41:59 +0700

    Tambah landing page

---
+++ application/controllers/Home.php
@@ -0,0 +1,21 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class Home extends CI_Controller
+{
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Pesantren_model');
+    }
+
+    public function index()
+    {
+
+
+
+        $this->load->view('home/index');
+    }
+}
